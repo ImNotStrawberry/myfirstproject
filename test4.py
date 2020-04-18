6d517a209fdcdf9916cd06f4a8446ee573088489
Morgane Austreelis <morgane.austreelis@gmail.com>
2020-04-18 23:18:24 +0000

    [test4.py] PEP 8 compliance + format string readability
    Comme pour le commit fc4ed04, j'ai rajouté une ligne vide à
    la fin du fichier, recomandée par PEP 8.
    
    Comme dans le commit 3bce0f7, j'ai remplacé les appels à
    `format()` (au lieu des sommes de `str`) par la syntaxe des
    formats-strings.
    
    PEP 8 recommande aussi de mettre des espaces autour des opérateurs (`=`,
    `==`, `+`, ...), que j'ai donc rajouté.
    
    Un autre recomendation de PEP 8 que j'ai déjà mentionné, c'est la limite
    de 80 caractères par ligne de code. Le but de cette recommandation est
    de limiter les codes "hyper compactes" qui essayent de faire le plus
    possible en le moins de lignes possibles, souvent considérés très peu
    lisibles, ou d'avoir à scroll comme pas possible si tu veux afficher
    beacoup de texte (par exemple).
    
    Dans `text4.py`, deux appels à `print()` excèdent cette limite, j'ai
    donc utilisé deux caractéristiques de la syntaxe python:
    - Les "string literals", c'est-à-dire le texte écrit directement entre
      guillemets, peut être concaténé sans opérateur `+` entre elles:
      ```
      >>> "Hello" " World" == "Hello World"
      True
      ```
      > Attention à ne pas oublier les espaces, ils ne sont pas rajoutés
    - Python s'attend à trouver une parenthèse pour délimiter la fin de
      l'appel, donc si tu reviens à la ligne il continue d'évaluer
      l'argument de la fonction:
      ```
      >>> str(5
      ...     + 4
      ...     + 2
      ...     -7
      ... )
      '4'
      ```
    
    Du coup tu peux concaténer des strings dans un appel de fonction juste
    en la terminant avec un guillemet, et en continuant à la ligne suivante.
    Combiner des formats-strings ne pose pas problème non plus.
    
    Signed-off-by: Morgane Austreelis <morgane.austreelis@gmail.com>

--- test4.py
+++ test4.py
@@ -1,10 +1,14 @@
 from random import shuffle
-text=input("Entrez des mots au hasard : ").split(" ")
-num=len(text)
-print("Votre liste contient {} mots".format(num))
+
+text = input("Entrez des mots au hasard : ").split()
+num = len(text)
+print(f"Votre liste contient {num} mots")
 shuffle(text)
 if num < 10:
     print("La liste a été mélangée.")
-    print("Voici les deux premiers termes de cette liste : {}, {}".format(text[0], text[1]))
+    print("Voici les deux premiers termes de cette liste : "
+          f"{text[0]}, {text[1]}")
 else:
-    print("Voici les trois derniers termes cette liste : {}, {}".format(text[-1], text[-2]), text[-3])
+    print("Voici les trois derniers termes cette liste : "
+          f"{text[-2]}, {text[-3]}")
+
