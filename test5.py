fae81d500fc5ee9d99d088b86c1aa4d322f3a3b4
ImNotStrawberry <anaelle.touzet@gmail.com>
2020-04-18 22:34:01 +0200

    test5.py

---
+++ test5.py
@@ -0,0 +1,15 @@
+from random import randint
+just_price=randint(1, 9999)
+run=True
+
+while run:
+    price=int(input("Entrez un nombre : "))
+    if price==just_price:
+        print("Félicitations !")
+        run=False
+    elif price>just_price:
+        print("Trop haut")
+    elif price<just_price:
+        print("Trop bas")
+
+print("Fin du jeu")
