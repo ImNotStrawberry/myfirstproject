e0faba6556240ebc0ea5a5f6bf7e44b1cb7f2a42
Morgane Austreelis <morgane.austreelis@gmail.com>
2020-04-18 23:49:23 +0000

    [test5.py] PEP 8 compliance
    Comme pour le commit fc4ed04, j'ai rajouté une ligne vide à
    la fin du fichier, recomandée par PEP 8.
    
    Comme dans le commit précédent (3bce0f7), j'ai rajouté des espaces
    autour des opérateurs, recommandés par PEP 8.
    
    Signed-off-by: Morgane Austreelis <morgane.austreelis@gmail.com>

--- test5.py
+++ test5.py
@@ -1,15 +1,17 @@
 from random import randint
-just_price=randint(1, 9999)
-run=True
+
+just_price = randint(1, 9999)
+run = True
 
 while run:
-    price=int(input("Entrez un nombre : "))
-    if price==just_price:
+    price = int(input("Entrez un nombre : "))
+    if price == just_price:
         print("Félicitations !")
-        run=False
-    elif price>just_price:
+        run = False
+    elif price > just_price:
         print("Trop haut")
-    elif price<just_price:
+    elif price < just_price:
         print("Trop bas")
 
 print("Fin du jeu")
+
