68fb143a8553fb29bd71586111efea9c475aa30d
Anaëlle Tzt <imnotstrawberry.github@outlook.com>
2020-04-23 00:08:12 +0200

    Changed input order

--- je_sais_pas_comment_nommer_ce_fichier.py
+++ je_sais_pas_comment_nommer_ce_fichier.py
@@ -1,6 +1,7 @@
-max = int(input("Entrez la limite : "))
 num = int(input("Entrez un nombre de départ : "))
 mult = int(input("Entrez le nombre par lequel vous souhaitez multiplier : "))
+max = int(input("Entrez la limite : "))
+
 print(num)
 
 while num < max:
