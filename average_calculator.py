3fc8731da88ed0dfa2aaf1864da5d0877a1d6ffb
Anaëlle Tzt <imnotstrawberry.github@outlook.com>
2020-04-22 21:31:12 +0200

    int -> float

--- average_calculator.py
+++ average_calculator.py
@@ -1,7 +1,7 @@
 print("/!\ Séparez les notes par des espaces et remplacez les virgules par des points /!\ ")
 notes  = input("Entrez vos notes : ").split()
 
-notes  = [int(i) for i in notes]
+notes  = [float(i) for i in notes]
 
 result = sum(notes) / len(notes)
 print(result)
